8c4bc431b42faca4d70b455348a3b082bf0e8d7a
Eyttao <415897434@qq.com>
2019-09-12 10:41:17 +0800

    1

---
+++ main.py
@@ -0,0 +1,10 @@
+import sys
+from PyQt5 import QtCore
+from PyQt5 import QtWidgets
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+if __name__ == '__main__':
+    pass
---
+++ mainWindowDesign.bat
@@ -0,0 +1,3 @@
+"D:\Program Files (x86)\JetBrains\PycharmProjects\pyqt_test\venv\Scripts\pyqt5designer.exe" MainWindow.ui
+"D:\Program Files (x86)\JetBrains\PycharmProjects\pyqt_test\venv\Scripts\python.exe" -m PyQt5.uic.pyuic MainWindow.ui -o QTgeneration/MainWindow.py
+exit
