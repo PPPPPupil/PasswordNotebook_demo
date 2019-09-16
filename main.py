1174a559b010118d7eb969e8b309d36d289599c1
Eyttao <415897434@qq.com>
2019-09-16 23:14:47 +0800

    table widget

--- main.py
+++ main.py
@@ -37,7 +37,6 @@ class AddWindow(QtWidgets.QWidget):
                                 self.Ui_Form.lineEdit_3.text())
         self.father_MainWindow.refresh_record()
 
-
     def on_push_Button2_clicked(self):
         """
         清空输入
@@ -65,12 +64,21 @@ class MainWindow(QtWidgets.QMainWindow):
         self.Ui_MainWindow.pushButton.clicked.connect(self.on_push_button_clicked)
         self.Ui_MainWindow.pushButton_2.clicked.connect(self.on_push_button2_clicked)
         self.Ui_MainWindow.pushButton_3.clicked.connect(self.on_push_button3_clicked)
-        self.Ui_MainWindow.listWidget.doubleClicked.connect(self.on_listWidget_double_clicked)
+        self.Ui_MainWindow.listWidget.doubleClicked.connect(self.on_list_Widget_double_clicked)
+        self.Ui_MainWindow.tableWidget.doubleClicked.connect(self.on_table_Widget_double_clicked)
         self.Ui_MainWindow.pushButton_4.clicked.connect(self.on_push_button4_clicked)
 
         # call
+        self.table_widget_init()
         self.show_all()
         self.Ui_MainWindow.lineEdit_4.setPlaceholderText("query all if nothing")
+
+    def table_widget_init(self):
+        self.Ui_MainWindow.tableWidget.setColumnCount(4)  # 设置table的列数
+        self.Ui_MainWindow.tableWidget.verticalHeader().setSectionsClickable(False)  # 表头不可点击
+        self.Ui_MainWindow.tableWidget.setHorizontalHeaderLabels(["id", "username", "password", "place"])  # 表头内容
+        self.Ui_MainWindow.tableWidget.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)  # 一次选一行
+        self.Ui_MainWindow.tableWidget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
 
     def show_all(self):
         """
@@ -80,6 +88,11 @@ class MainWindow(QtWidgets.QMainWindow):
         self.refresh_record()
         # self.Ui_MainWindow.listWidget.addItem("asdhasjkhf")
 
+    def remove_all_tableW_row(self):
+        current_row = self.Ui_MainWindow.tableWidget.rowCount()
+        for r in range(0, current_row)[::-1]:  # 倒序是关键
+            self.Ui_MainWindow.tableWidget.removeRow(r)
+
     def refresh_record(self, place=None):
         """
         刷新listView，（可传入条件place）
@@ -88,10 +101,18 @@ class MainWindow(QtWidgets.QMainWindow):
         """
         result = self.my_conn.query_records(place)
         self.Ui_MainWindow.listWidget.clear()
+        self.remove_all_tableW_row()
         for item in result:
             self.Ui_MainWindow.listWidget.addItem(str(item))
+            # table widget
+            current_row = self.Ui_MainWindow.tableWidget.rowCount()
+            self.Ui_MainWindow.tableWidget.insertRow(current_row)
+            self.Ui_MainWindow.tableWidget.setItem(current_row, 0, QtWidgets.QTableWidgetItem(str(item[0])))
+            self.Ui_MainWindow.tableWidget.setItem(current_row, 1, QtWidgets.QTableWidgetItem(item[1]))
+            self.Ui_MainWindow.tableWidget.setItem(current_row, 2, QtWidgets.QTableWidgetItem(item[2]))
+            self.Ui_MainWindow.tableWidget.setItem(current_row, 3, QtWidgets.QTableWidgetItem(item[3]))
 
-    def on_listWidget_double_clicked(self, e):
+    def on_list_Widget_double_clicked(self, e):
         """
         双击listView事件，读取该条记录的信息，并显示在面板上
         :param e:
@@ -105,6 +126,21 @@ class MainWindow(QtWidgets.QMainWindow):
         self.Ui_MainWindow.lineEdit_2.setText(_data[2])
         self.Ui_MainWindow.lineEdit_3.setText(_data[3])
 
+    def on_table_Widget_double_clicked(self, e):
+        """
+        双击listView事件，读取该条记录的信息，并显示在面板上
+        :param e:
+        :return:
+        """
+
+        # print(e.data())  # 点击事件以item为单位（can not row）
+        selected_row = e.row()  # 获取当前点击item所在的行数
+        # _data = eval(e.data()[1])
+        self.current_id = self.Ui_MainWindow.tableWidget.item(selected_row, 0).text()
+        self.Ui_MainWindow.lineEdit.setText(self.Ui_MainWindow.tableWidget.item(selected_row, 1).text())
+        self.Ui_MainWindow.lineEdit_2.setText(self.Ui_MainWindow.tableWidget.item(selected_row, 2).text())
+        self.Ui_MainWindow.lineEdit_3.setText(self.Ui_MainWindow.tableWidget.item(selected_row, 3).text())
+
     def on_push_button_clicked(self):
         """
         修改当前所显示的信息（先双击，再修改）
@@ -116,7 +152,6 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.my_conn.modify_record(username, password, place, self.current_id)
         self.refresh_record()
-        print("refresh_record")
 
     def on_push_button3_clicked(self):
         """
